611edf257aded37e300569f00c49ba46b52787ee
fahimfaysalniloy <fahimfaysal.buetbd@gmail.com>
2020-08-04 23:58:45 +0600

    modified form submit and attached it to backend

---
+++ DeeWhy/Js/feedback.js
@@ -0,0 +1,7 @@
+const msg = document.getElementById('NameOfCustomer').value;
+const submitBtn = document.getElementById('save');
+
+submitBtn.addEventListener(e =>{
+    const msg = document.getElementById('NameOfCustomer').value;
+    console.log(msg);
+}) 
